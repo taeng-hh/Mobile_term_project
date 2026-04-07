6472b5c1224b207887a56b3b40bbdc4cba907ad0
Kim-Soo-A <kimsooa0912@naver.com>
2026-04-07 11:05:10 +0900

    로그아웃 팝업

--- app/src/main/java/com/example/term_project/MainFragment.java
+++ app/src/main/java/com/example/term_project/MainFragment.java
@@ -13,6 +13,9 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 
 public class MainFragment extends Fragment {
 
@@ -86,11 +89,42 @@ public class MainFragment extends Fragment {
         Button btnSound = view.findViewById(R.id.btnSound);
         Button btnHelp = view.findViewById(R.id.btnHelp);
         Button btnClosePopup = view.findViewById(R.id.btnClosePopup);
+        Button logoutBtn = view.findViewById(R.id.logoutBtn);
 
         // 설정 열기
         btnSettings.setOnClickListener(v -> {
             dimView.setVisibility(View.VISIBLE);
             settingsPopup.setVisibility(View.VISIBLE);
+        });
+
+        // 로그아웃 버튼(팝업으로 한 번 더 확인 후 로그아웃)
+        logoutBtn.setOnClickListener(v -> {
+
+            new AlertDialog.Builder(requireContext())
+                    .setTitle("로그아웃")
+                    .setMessage("정말 로그아웃 하시겠습니까?")
+
+                    .setPositiveButton("예", (dialog, which) -> {
+
+                        // 자동으로 팝업 닫고 로그아웃
+                        dimView.setVisibility(View.GONE);
+                        settingsPopup.setVisibility(View.GONE);
+
+                        SharedPreferences pref = requireActivity()
+                                .getSharedPreferences("user", getContext().MODE_PRIVATE);
+                        SharedPreferences.Editor editor = pref.edit();
+
+                        editor.putBoolean("isLogin", false);
+                        editor.apply();
+
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                        requireActivity().finish();
+                    })
+
+                    .setNegativeButton("아니오", null)
+
+                    .show();
         });
 
         // 닫기 버튼
